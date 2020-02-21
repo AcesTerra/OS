ec3cd6b57cb28b3d7db62b03c55ddc3f0cf73833
Alfredo Emmanuel Garcia Falcon <jovengf@gmail.com>
2020-02-21 04:13:51 -0600

    Code cleaned
    Signed-off-by: Alfredo Emmanuel Garcia Falcon <jovengf@gmail.com>

--- homework/homehork01/raiz/main.cpp
+++ homework/homehork01/raiz/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	//Checking correct arguments
 	if(argc != 2)
 	{
 		printf("Must have one argument\n");
@@ -19,63 +20,43 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	//printf("%lu\n", sizeof(int));
-	//printf("%lu\n", sizeof(double));
-	//printf("%lu\n", sizeof(char[50]));
-
+	//Initializing array of struct
 	int files = stoi(argv[1]);
 	string extension = ".dat";
 	data *allData;
   	int dataSize = sizeof(data);
-  	//a = (struct data*) malloc(ds);
   	allData = (struct data*) malloc(dataSize*files);
+	//Extract data from files
 	for(int i = 0; i < files; i++){
 		string fileName = to_string(i+1) + extension;
-		//cout << fileName << endl;
 		char * charFileName = new char [fileName.length()];
 		strcpy (charFileName, fileName.c_str());
-		//allData[i] = extractData(fileNumber);
-		//cout << "About to enter extractData" << endl;
-		//allData[i] = extractData(charFileName);
 		allData[i] = extractData(charFileName);
 	}
 
+	//Printing data extracted from files
 	for(int i = 0; i < files; i++)
 		printf("Data from the file %d.dat: %d %d %0.2lf %s\n", i+1, (allData+i)->x, (allData+i)->y, (allData+i)->z, (allData+i)->name);
 
+	//Char array to copy data
 	char *result;
   	result = (char*) malloc(dataSize*files);
   	memcpy(&result, &allData, dataSize*files);
-	//printf("Data of result: %s\n", result);
 
 	FILE *fptr;
 
-	// opening file in writing mode
+	//Opening file in writing binary mode
 	fptr = fopen("resultados/datos.bin", "wb");
 
-	// exiting program
+	//Exiting program if file is not created
 	if (fptr == NULL) {
 		printf("Error!");
 		exit(1);
 	}
-	//cout << "Opening file" << endl;
-	//printf("Enter a sentence:\n");
-	//fgets(sentence, sizeof(sentence), stdin);
-	//fprintf(fptr, "%s", result);
+
 	fwrite(result, sizeof(result), 1, fptr);
-	printf("%lu\n", sizeof(*allData));
 	cout << "Writing into file" << endl;
 	fclose(fptr);
-	//cout << "Closing file" << endl;
 
-	/*data *recover;
-  	recover = (struct data*) malloc(dataSize*files);
-  	memcpy(&recover, &result, dataSize*files);
-
-	for(int i = 0; i < files; i++)
-		printf("Data from recover %d: %d %d %0.2lf %s\n", i+1, (recover+i)->x, (recover+i)->y, (recover+i)->z, (recover+i)->name);*/
-
-	//printf("%d,%d,%f\n",recover->x,recover->y,recover->res);
-	//printf("%d,%d,%f\n",(recover+1)->x,(recover+1)->y,(recover+1)->res);
 	return 0;
 }
